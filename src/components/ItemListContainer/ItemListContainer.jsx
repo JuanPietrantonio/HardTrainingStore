ca366eff29a62a1bc909b5cd3de316f6fbd2ebc8
JuanPietrantonio <juampipietra@gmail.com>
2022-12-01 22:45:23 -0300

    Proyecto final Pietrantonio Juan Pablo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -10,6 +10,7 @@ function ItemListContainer() {
                 <Item title="algo" imgurl="https://http2.mlstatic.com/D_NQ_NP_630166-MLA45787954825_052021-O.webp" price="100" detail="detalles"></Item>
                 <Item title="algo12" price="200" detail="detalles"></Item>
                 <Item title="algo23" price="300" detail="detalles"></Item>
+                <Item title="algo234" price="300" detail="detalles"></Item>
         </FlexWrapper>
     </div>
   )
